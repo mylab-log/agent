54ffa57b55a1e6b39a3a14807c9c894b87ebeed9
Andrey O <ozzy.ext@gmail.com>
2023-08-22 21:58:06 +0300

    Lua filter implementation

--- src/lua-filter/yaml-filter.lua
+++ src/lua-filter/yaml-filter.lua
@@ -2,7 +2,72 @@ local lyaml = require "lyaml"
 
 function extract_all(tag, timestamp, record)
 
-    
+    local log_rec = record["log"]
 
-    return code, timestamp, record
+    if log_rec == nil then 
+        return 0, timestamp, record
+    end
+
+    local doc = lyaml.load(log_rec)
+    
+    if doc == nil then 
+        return 0, timestamp, record
+    end
+
+    extract_facts(doc["Facts"], record)
+    extract_exception(doc["Exception"], record)
+    
+    local labels = doc["Labels"]
+    extract_loglevel(labels, record)
+    extract_labels(labels, record)
+
+    return 2, timestamp, record
+end
+
+local funcion extract_facts(doc, record)
+
+    local facts = doc["Facts"]
+    if facts ~= nil then
+        local facts_str = lyaml.dump(facts)
+        record["facts"] = facts_str
+    end
+
+end
+
+local funcion extract_exception(doc, record)
+
+    local ex = doc["Exception"]
+    if ex ~= nil then
+        local ex_str = lyaml.dump(ex)
+        record["exception"] = ex_str
+    end
+
+end
+
+local funcion extract_lavel(labels, record)
+
+    local lvl1 = labels["log_level"]
+    local lvl2 = labels["log-level"]
+    local lvl = lvl1
+
+    if lvl == nil then
+        lvl = lvl2
+    end
+
+    if lvl ~= nil then
+        record["level"] = lvl
+    else
+        record["level"] = "info"
+    end
+
+end
+
+local funcion extract_labels(labels, record)
+
+    for n,v in ipairs(labels) do
+        if n ~= "log_level" and n ~= "log-level" then
+            record[n] = v
+        end
+    end
+
 end
--- test/test.ps1
+++ test/test.ps1
@@ -1,24 +1,12 @@
-$srcPath = Resolve-Path ..\src
 $curPath = Resolve-Path .
 
 docker run `
-    -v $srcPath\lua-filter\yaml-filter.lua:/test/yaml-filter.lua:ro `
     -v $curPath\test.log:/test/test.log `
     -v $curPath\input.conf:/fluent-bit/etc/includes/input.conf:ro `
     -v $curPath\output.conf:/fluent-bit/etc/includes/output.conf:ro `
     -v $curPath\common-final.conf:/fluent-bit/etc/includes/common-final.conf:ro `
     -e MLAGENT_HOST=test `
     -e MLAGENT_ENV=test `
-    --rm `
-    -ti `
-    mylab-log/agent:debug
-
-    $curPath = Resolve-Path .
-
-docker run `
-    -v $curPath\yaml-filter.lua:/test/yaml-filter.lua:ro `
-    -v $curPath\test.log:/test/test.log:ro `
-    -v $curPath\test.conf:/fluent-bit/etc/fluent-bit.conf:ro `
     --rm `
     -ti `
     mylab-log/agent:debug
