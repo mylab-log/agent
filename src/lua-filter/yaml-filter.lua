e72f438ec63dfe8c476f265342d73cacfd13fafd
Andrey O <ozzy@triasoft.com>
2023-08-27 22:42:26 +0300

    Add json for debugging

--- src/lua-filter/yaml-filter.lua
+++ src/lua-filter/yaml-filter.lua
@@ -1,40 +1,33 @@
 local lyaml = require "lyaml"
+local json = require "json"
 
-function extract_all(tag, timestamp, record)
+local function extract_message(doc, record)
 
-    local log_rec = record["log"]
-
-    if log_rec == nil then 
-        return 0, timestamp, record
+    local msg = doc["Message"]
+    if msg ~= nil then
+        record["message"] = msg
     end
 
-    local doc = lyaml.load(log_rec)
-    
-    if doc == nil then 
-        return 0, timestamp, record
-    end
-
-    extract_facts(doc["Facts"], record)
-    extract_exception(doc["Exception"], record)
-    
-    local labels = doc["Labels"]
-    extract_loglevel(labels, record)
-    extract_labels(labels, record)
-
-    return 2, timestamp, record
 end
 
-local funcion extract_facts(doc, record)
+local function extract_facts(doc, record)
 
     local facts = doc["Facts"]
     if facts ~= nil then
+        
+        factsArr = { facts }
+        
+        print("Facts >>>")
+        -- print(json.encode(facts))
+        print(lyaml.dump(factsArr))
+        print("Facts <<<")
         local facts_str = lyaml.dump(facts)
         record["facts"] = facts_str
     end
 
-end
+end 
 
-local funcion extract_exception(doc, record)
+local function extract_exception(doc, record)
 
     local ex = doc["Exception"]
     if ex ~= nil then
@@ -44,7 +37,7 @@ local funcion extract_exception(doc, record)
 
 end
 
-local funcion extract_lavel(labels, record)
+local function extract_level(labels, record)
 
     local lvl1 = labels["log_level"]
     local lvl2 = labels["log-level"]
@@ -62,7 +55,7 @@ local funcion extract_lavel(labels, record)
 
 end
 
-local funcion extract_labels(labels, record)
+local function extract_labels(labels, record)
 
     for n,v in ipairs(labels) do
         if n ~= "log_level" and n ~= "log-level" then
@@ -70,4 +63,31 @@ local funcion extract_labels(labels, record)
         end
     end
 
+end
+
+function extract_all(tag, timestamp, record)
+    
+    local log_rec = record["log"]
+
+    if log_rec == nil then 
+        return 0, timestamp, record
+    end
+
+    local doc = lyaml.load(log_rec)
+    
+    if doc == nil then 
+        return 0, timestamp, record
+    end
+
+    extract_message(doc, record)
+    extract_facts(doc, record)
+    extract_exception(doc, record)
+    
+    local labels = doc["Labels"]
+    if labels ~= nil then
+        extract_level(labels, record)
+        extract_labels(labels, record)
+    end
+
+    return 2, timestamp, record
 end
