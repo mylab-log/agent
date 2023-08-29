7468b0834f4cc617378de055d7d9ab0f139ef93e
Andrey O <ozzy.ext@gmail.com>
2023-08-29 23:35:04 +0300

    Support default multiline

--- test/test-container.ps1
+++ test/test-container.ps1
@@ -1,10 +1,9 @@
 $curPath = Resolve-Path .
 
 docker run `
-    -v $curPath\test.log:/test/test.log `
+    -v $curPath\$($args[0]).log:/test/test.log `
     -v $curPath\input.conf:/fluent-bit/etc/includes/input.conf:ro `
     -v $curPath\output.conf:/fluent-bit/etc/includes/output.conf:ro `
-    -v $curPath\common-final.conf:/fluent-bit/etc/includes/common-final.conf:ro `
     -e MLAGENT_HOST=test `
     -e MLAGENT_ENV=test `
     --rm `
