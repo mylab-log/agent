ca0b53f91f4939286dd9bc0a607c24c3ad34c1b2
Andrey O. <ozzy.ext@gmail.com>
2023-09-18 17:08:07 +0300

    Add script test

---
+++ test/test-script.ps1
@@ -0,0 +1,13 @@
+$curPath = Resolve-Path .
+$srcPath = Resolve-Path ..\src
+
+docker run `
+    -v $curPath\$($args[0]).log:/test/test.log `
+    -v $curPath\input.conf:/fluent-bit/etc/includes/input.conf:ro `
+    -v $curPath\output.conf:/fluent-bit/etc/includes/output.conf:ro `
+    -v $srcPath\lua-filter\yaml-filter.lua:/usr/lib/mylab-logagent/filters/yaml-filter.lua:ro `
+    -e MLAGENT_HOST=test `
+    -e MLAGENT_ENV=test `
+    --rm `
+    -ti `
+    mylab-log/agent:debug
