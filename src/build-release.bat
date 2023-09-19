923c4f8d24b6b1afd2d1002b12441363b4e4af3a
Andrey O <ozzy@triasoft.com>
2023-09-19 19:51:46 +0300

    Add metrcs providing

--- src/build-release.bat
+++ src/build-release.bat
@@ -4,7 +4,7 @@ IF [%1]==[] goto noparam
 
 echo "Build image '%1'"
 
-docker build --build-arg MLAGNET_VER=%1 --build-arg FLUENT_BIT_VER=2.0.9 -t ghcr.io/mylab-log/agent:%1 -t ghcr.io/mylab-log/agent:latest ../src
+docker build --build-arg MLAGENT_VER=%1 --build-arg FLUENT_BIT_VER=2.0.9-debug -t ghcr.io/mylab-log/agent:%1 -t ghcr.io/mylab-log/agent:latest ../src
 
 echo "Publish image '%1' ..."
 docker push ghcr.io/mylab-log/agent:%1
