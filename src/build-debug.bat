1e5eecdc9166419da8daa18eb619f474552e5483
Andrey O. <ozzy.ext@gmail.com>
2023-09-22 18:09:02 +0300

    Fix MLAGENT build arg passing

--- src/build-debug.bat
+++ src/build-debug.bat
@@ -1,8 +1,7 @@
 @echo off
 
-set tag=ghcr.io/mylab-log/agent
-
 echo "Build image ..."
 @REM set DOCKER_BUILDKIT=0
-docker build --build-arg MLAGNET_VER=%1 --build-arg FLUENT_BIT_VER=2.1.8-debug -t mylab-log/agent:debug ../src
+
+docker build --build-arg MLAGENT_VER=debug --build-arg FLUENT_BIT_VER=2.1.8-debug -t mylab-log/agent:debug ../src
 echo "Done!"
