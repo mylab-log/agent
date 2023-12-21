e27f98d03a50a435b33a6ca4a88096e5f8958ca1
ozzy-ext <ozzy.ext@gmail.com>
2023-12-21 19:02:08 +0300

    Create initial solution and remove old sources

--- src/build-debug.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo "Build image ..."
-@REM set DOCKER_BUILDKIT=0
-
-docker build --build-arg MLAGENT_VER=debug --build-arg FLUENT_BIT_VER=2.1.8-debug -t mylab-log/agent:debug ../src
-echo "Done!"
